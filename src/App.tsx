aaec8e5d24e73e67a13b2a90c71614ba06be84e3
Prashant Khanal <prashantkhanal32@gmail.com>
2022-05-15 12:08:09 +0545

    chore(All): project creation

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// ehehe
+
+/**
+ * Main app compinet and returns the
+ * @author prashant
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <div className="">Vidhyarthi</div>
+    </div>
+  );
+}
+
+export default App;
