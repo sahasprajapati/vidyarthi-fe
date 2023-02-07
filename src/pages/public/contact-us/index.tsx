1cfd08e057e8c35f888e447001db1abf19c7862b
Prashant Khanal <prashantkhanal32@gmail.com>
2023-02-07 18:42:44 +0545

    fix(all): feedback upadted

--- src/pages/public/contact-us/index.tsx
+++ src/pages/public/contact-us/index.tsx
@@ -38,38 +38,12 @@ const ContactUs: React.FC<IProps> = ({}) => {
   };
   return (
     <React.Fragment>
+      <div className="banner-container">
+        <NavBar variant="white" />
+        <h5 className="banner-container-text">Contact Us</h5>
+      </div>
       <div className="container">
-        <NavBar variant="black" />
-        <Heading title={'Contact Us'} className="text-center mb-5" />
-        <p className="f-14">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
-          soluta? Quas, impedit rem qui consectetur atque optio eveniet, soluta
-          ab maiores cum et saepe totam fuga amet aspernatur quis similique
-          voluptatibus voluptatum ipsa magni. Architecto blanditiis accusantium
-          laborum. Vero excepturi totam voluptas dolorum ipsam dolores,
-          dignissimos tenetur quod libero amet quam! Accusantium magnam eos,
-          ipsum aut cupiditate quas neque, consectetur reprehenderit quos
-          dolorem enim excepturi amet voluptatibus blanditiis eligendi illo
-          voluptatum, et impedit? Sunt dolorum temporibus recusandae modi
-          asperiores corporis! Cumque, dolorum culpa. Corrupti magnam
-          blanditiis, eveniet incidunt at ducimus iusto explicabo qui ipsam
-          suscipit tempora quo, minus soluta impedit similique doloribus dolorum
-          optio. Voluptatem quo magni dolores distinctio dolorum, earum id
-          similique dicta eligendi porro adipisci veniam deleniti iusto animi
-          aperiam necessitatibus molestiae corporis tenetur. Pariatur ipsum
-          architecto accusamus debitis, sequi dolorum alias saepe ex provident
-          velit? Ea doloribus error dicta culpa commodi ab accusantium repellat
-          quos totam ullam consequuntur labore nam quis repellendus
-          exercitationem aliquam at, magnam odit repudiandae? Soluta cupiditate
-          error rerum nulla consequuntur exercitationem odit deleniti,
-          distinctio architecto est sed quas nesciunt asperiores veritatis
-          voluptas? Nostrum incidunt autem quam laboriosam blanditiis nam porro,
-          error saepe eos veniam ad nesciunt dolores qui! Deleniti dolores modi
-          ipsa possimus.
-        </p>
-        <p className="text-center f-18-f500 mt-5">
-          Contact Us for more inforamtion
-        </p>
+        <h3 className="text-center my-5">Contact Us for more inforamtion</h3>
         <div className="row mt-3 mb-5">
           <div className="col-lg-6 col-sm-12 col-12 mx-auto">
             <Formik
@@ -100,11 +74,20 @@ const ContactUs: React.FC<IProps> = ({}) => {
                     name="address"
                     placeholder="Enter your Address"
                   />
-                  <TextField
-                    label={'Comment'}
-                    name="comment"
-                    placeholder="Enter your Address"
-                  />
+                  <div className="my-4">
+                    <label htmlFor="" className="mb-2">
+                      Comment
+                    </label>
+
+                    <textarea
+                      name="comment"
+                      cols={70}
+                      rows={5}
+                      placeholder="Please write down your feed back here.."
+                      className="p-4 contact-comment"
+                    />
+                  </div>
+
                   <div className="mt-5">
                     <Button
                       variant="primary"
